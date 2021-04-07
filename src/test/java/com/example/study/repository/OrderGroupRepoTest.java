6b4434d9a314c913dda1d8b42009e6bc5da3af5e
shkimm5189 <shkimm5189@gmail.com>
2021-04-07 16:00:36 +0900

    TEST : OrderGroup , AdminUser table test

---
+++ src/test/java/com/example/study/repository/OrderGroupRepoTest.java
@@ -0,0 +1,38 @@
+package com.example.study.repository;
+
+import com.example.study.StudyApplicationTests;
+import com.example.study.model.entity.OrderGroup;
+import com.example.study.repogitory.OrderGroupRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class OrderGroupRepoTest extends StudyApplicationTests {
+
+    @Autowired
+    private OrderGroupRepository orderGroupRepository;
+
+    @Test
+    public void create(){
+        OrderGroup orderGroup = new OrderGroup();
+
+        orderGroup.setStatus("COMPLETE");
+        orderGroup.setOrderType("CARD");
+        orderGroup.setRevAddress("서울시 강남구");
+        orderGroup.setRevName("홍길동");
+        orderGroup.setPaymentType("CARD");
+        orderGroup.setTotalPrice(BigDecimal.valueOf(9000000));
+        orderGroup.setTotalQuantity(1);
+        orderGroup.setOrderAt(LocalDateTime.now().minusDays(2));
+        orderGroup.setArrivalDate(LocalDateTime.now());
+        orderGroup.setCreatedAt(LocalDateTime.now());
+        orderGroup.setCreatedBy("Partner01");
+        orderGroup.setUserId(1L);
+
+        OrderGroup newOrderGroup = orderGroupRepository.save(orderGroup);
+        Assertions.assertNotNull(newOrderGroup);
+    }
+}
